bbdae111ef222183c2d866e3be589d40970fc790
Turabi Sancak <muhammedturabisancak@gmail.com>
2024-09-14 19:26:40 +0300

    Initial Commit

---
+++ FavoritesButtonAnimation/FavoritesButtonAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesButtonAnimationApp.swift
+//  FavoritesButtonAnimation
+//
+//  Created by Muhammed Turabi Sancak on 14.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct FavoritesButtonAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FavoritesButton()
+        }
+    }
+}
